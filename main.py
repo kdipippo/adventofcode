dd96078c79d63ea71a6093ed37fe72409c82ff8d
kdipippo <kathryn.dipippo@acquia.com>
2023-12-06 18:07:22 -0500

    WIP 2023 Day 5 part 2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from array import array
+
 def read_file_into_words(filename: str) -> list:
     with open(filename) as f:
         content = f.readlines()
@@ -29,22 +31,37 @@ class SeedsTracker:
             if map_row[1] <= num < (map_row[1] + map_row[2]):
                 # Calculate mapping.
                 new_num = map_row[0] + (num - map_row[1])
-                print(f"Convert {num} -> {new_num}")
+                # print(f"Convert {num} -> {new_num}")
                 self.after_nums.append(new_num)
                 self.before_nums.remove(num)
 
     def migrate_rows_after_processing(self):
-        print(f"Passing {', '.join([str(entry) for entry in self.before_nums])}")
+        # print(f"Passing {', '.join([str(entry) for entry in self.before_nums])}")
         for entry in self.after_nums:
-            self.before_nums.append(entry)
+            if entry not in self.before_nums:
+                self.before_nums.append(entry)
         self.after_nums = []
 
 def parse_seeds(seeds_str):
-    return [int(entry) for entry in seeds_str.replace("seeds: ", "").split()]
+    split_list = [int(entry) for entry in seeds_str.replace("seeds: ", "").split()]
+    seeds = []
+    for index in range(0, len(split_list), 2):
+        seeds.append(split_list[index])
+        seeds.append(split_list[index] + split_list[index+1])
+    return seeds
+
+def parse_seeds2(seeds_str):
+    split_list = [int(entry) for entry in seeds_str.replace("seeds: ", "").split()]
+    seeds = array('L', [])
+    for index in range(0, len(split_list), 2):
+        print(f"  Range #{index} out of {len(split_list)}")
+        seeds.extend(array('L', [i for i in range(split_list[index], split_list[index] + split_list[index+1])]))
+    return seeds
 
 def main():
     inputs = read_file_into_words("fullinput.txt")
     grouped_instructions = convert_to_2d_list(inputs)
+    print("Loading seeds")
     seeds_tracker = SeedsTracker(grouped_instructions[0][0])
     for instr_index in range(1, len(grouped_instructions)):
         print(f"\nProcessing '{grouped_instructions[instr_index][0]}'")
